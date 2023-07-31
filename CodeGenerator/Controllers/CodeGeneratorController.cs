a92eef9082d1b66946453e4fed9177d8f4d83fc6
nursenkrac <nursenkrac@gmail.com>
2023-07-31 12:55:31 +0300

    Add project files.

---
+++ CodeGenerator/Controllers/CodeGeneratorController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace CodeGenerator.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CodeGeneratorController : Controller
+    {
+    
+        [HttpGet]
+        public string GetCode()
+        {
+           return GenerateUniqueCode();
+            
+        }
+
+        public static string GenerateUniqueCode()
+        {
+            Random ran = new Random(DateTime.Now.Millisecond);
+            const string chars = "ACDEFGHKLMNPRTXYZ234579";
+            return new string(Enumerable.Repeat(chars, 8)
+                .Select(s => s[ran.Next(s.Length)]).ToArray());
+        }
+    }
+}
+   
+
